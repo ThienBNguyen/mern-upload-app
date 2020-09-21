1c199840339ec1ea29348782ea63ff2ea5bd0b06
Thien Nguyen <thien.nguyen30191@gmail.com>
2020-09-20 21:05:00 -0500

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+export default class App extends Component {
+  Post = e => {
+    e.preventDefault();
+    const file = document.getElementById("inputGroupFile01").files;
+    const formData = new FormData();
+
+    formData.append("img", file[0]);
+
+    fetch("http://localhost:5000/", {
+      method: "POST",
+      body: formData
+    }).then(r => {
+      console.log(r);
+    });
+    console.log(file[0]);
+  };
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="jumbotron">
+            <h1 className="display-4">Image Uplaoder</h1>
+            <p className="lead">
+              This is a simple application to upload and retrieve images from a
+              database
+            </p>
+            <hr className="my-4" />
+          </div>
+          <div className="input-group mb-3">
+            <div className="custom-file">
+              <input
+                type="file"
+                className="custom-file-input"
+                id="inputGroupFile01"
+                aria-describedby="inputGroupFileAddon01"
+              />
+              <label className="custom-file-label" htmlFor="inputGroupFile01">
+                Choose file
+              </label>
+            </div>
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.Post()}
+          >
+            Upload
+          </button>
+          <img
+            id="img"
+            style={{
+              display: "block"
+            }}
+          ></img>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
